d19b298d460b4600868dfd7c3d1fa3c68d58c555
Donut <joshchia1227@gmail.com>
2019-07-01 22:54:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/joshc/firebase_project/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.joshc.firebase_project;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edt_Account;
+    private EditText edt_Password;
+    private Button btn_Login;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        edt_Account = (EditText) findViewById(R.id.edt_Account);
+        edt_Password = (EditText) findViewById(R.id.edt_Password);
+        btn_Login = (Button) findViewById(R.id.btn_Login);
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login(){
+        String email = edt_Account.getText().toString();
+        String password = edt_Password.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("msg", "signInWithEmail:success");
+                        }else{
+                            Log.d("msg","error");
+                        }
+                    }
+                });
+    }
+}
