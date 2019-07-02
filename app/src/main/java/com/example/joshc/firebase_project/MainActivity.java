382f48816ef480cc8d93c4f911e0ae965394651e
Donut <joshchia1227@gmail.com>
2019-07-02 12:52:17 +0800

    login feild wrong

--- app/src/main/java/com/example/joshc/firebase_project/MainActivity.java
+++ app/src/main/java/com/example/joshc/firebase_project/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("msg", "signInWithEmail:success");
                         }else{
-                            Log.d("msg","login feild ");
+                            Log.d("msg","login feild  wrong");
                         }
                     }
                 });
